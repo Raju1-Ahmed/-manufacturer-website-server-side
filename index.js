deafef0a2a7dc489f0612bd85d61645f02b8608e
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-05-24 21:46:41 +0600

    order api add

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 
 const port = process.env.PORT || 5000;
@@ -14,26 +15,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
-      await client.connect();
-      const productsCollection = client.db('Auto_Parts').collection('Products');
+  try {
+    await client.connect();
+    const productsCollection = client.db('Auto_Parts').collection('Products');
+    const orderCollection = client.db('Auto_Parts').collection('Clint_Order');
 
-      app.get('/products', async (req, res) => {
-        const query = {};
-        const cursor = productsCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-      });
+    //Get All products api
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    } finally {
-    }
+    //Get a single id
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await productsCollection.findOne(query);
+      res.send(service);
+    })
+    // order api
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello From AutoParts!')
+  res.send('Hello From AutoParts!')
 })
 
 app.listen(port, () => {
-    console.log(`Example AutoParts listening on port ${port}`)
+  console.log(`Example AutoParts listening on port ${port}`)
 })
